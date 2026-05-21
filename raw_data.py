f03be7b62864dffdd675f0ecb0a8cbb8a0bd7ab8
noorulhassanessex <nicestimescomes@gmail.com>
2026-05-21 21:18:05 +0100

    Update raw_data.py

--- raw_data.py
+++ raw_data.py
@@ -3,7 +3,7 @@ import os
 
 def read_data():
     if not os.path.exists("raw_data.csv"):
-        print("⚠️ No raw_data.csv file found. Please enter data first.")
+        print(" No raw_data.csv file found. Please enter data first.")
         return []
     df = pd.read_csv("raw_data.csv")
     return df['Value'].tolist()
